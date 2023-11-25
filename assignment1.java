5a8ad31bdaac775d42f375c6118ddf56ba24ae22
suraj sakhare <sakharesuraj10@gmail.com>
2023-11-25 14:11:35 +0530

    make assignment1

--- assignment1.java
+++ assignment1.java
@@ -5,6 +5,7 @@ public class assignment1 {
 
     
     public static void main(String[] args) {
-        System.out.println("Hello World77");
+        System.out.println("Hello World12");
+        System.out.println("Hello World34");
     }
 }
